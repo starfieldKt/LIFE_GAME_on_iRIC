049a3f3678d8cf3d6be95b0238f0d0040f5c9922
KeitaHoshino <106499621+starfieldKt@users.noreply.github.com>
2025-03-19 10:54:23 +0900

    諸々のファイルを追加してバージョン更新

---
+++ translation_ja_JP.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+    <!--これは iRIC で使用する辞書ファイルです。
+<translation> と </translation> の間に、翻訳後の文字列を追記してください。
+-->
+    <context>
+        <message>
+            <source>LIFE GAME on iRIC</source>
+            <translation>ライフゲーム on iRIC</translation>
+        </message>
+        <message>
+            <source>Condition</source>
+            <translation>計算条件</translation>
+        </message>
+        <message>
+            <source>time end</source>
+            <translation>計算終了時刻</translation>
+        </message>
+        <message>
+            <source>periodic boundary condition</source>
+            <translation>周期境界条件</translation>
+        </message>
+        <message>
+            <source>Life</source>
+            <translation>生死</translation>
+        </message>
+        <message>
+            <source>Dead</source>
+            <translation>死亡</translation>
+        </message>
+        <message>
+            <source>Alive</source>
+            <translation>生存</translation>
+        </message>
+        <message>
+            <source>life</source>
+            <translation>生死</translation>
+        </message>
+    </context>
+</TS>
